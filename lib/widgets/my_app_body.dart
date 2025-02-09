53841215a7ebcff38669efbb563fd9396d02f22b
baselhegazi <baselmohammed226@gmail.com>
2025-02-09 14:34:04 +0200

    add ( key: scaffoldKey,             drawer: const CustomDrawer(),) and import required link paths

--- lib/widgets/my_app_body.dart
+++ lib/widgets/my_app_body.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'custom_drawer.dart';
 import 'custom_sliver_grid.dart';
 import 'custom_sliver_list.dart';
 
-class MyAppBody extends StatelessWidget {
+class MyAppBody extends StatefulWidget {
   const MyAppBody({super.key});
 
+  @override
+  State<MyAppBody> createState() => _MyAppBodyState();
+}
+
+class _MyAppBodyState extends State<MyAppBody> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
+      key: scaffoldKey,
+      drawer: const CustomDrawer(),
       appBar: AppBar(
         backgroundColor: Colors.white,
-        leading: Icon(
-          Icons.menu_outlined,
-          color: Colors.black,
-          size: 32,
+        leading: IconButton(
+          onPressed: () {
+            scaffoldKey.currentState!.openDrawer();
+          },
+          icon: Icon(
+            Icons.menu_outlined,
+            color: Colors.black,
+            size: 32,
+          ),
         ),
       ),
       body: Padding(
